17df6ac72619032c74dd6add8a9d6ae4db3b913e
h31nr1ch <tiagoheinrich1995@gmail.com>
2017-06-16 19:40:59 -0300

    R and html

--- r-database/application/run.R
+++ r-database/application/run.R
@@ -1,3 +1,6 @@
+#by h31nr1ch
+##dont change this
+
 #call with sourch('run.R')
 library(shiny)
-runApp("~/Udesc/github/Shiny-R-server/r-database/application/server.R")
+runApp("~/Udesc/Shiny-R-server/r-database/application/")
