327826dec76f9528585c1b5166d4a8b727a739e4
h31nr1ch <tiagoheinrich1995@gmail.com>
2017-06-18 12:53:35 -0300

    just update server

--- r-database/application/server.R
+++ r-database/application/server.R
@@ -4,66 +4,55 @@ library('xtable')
 library('DBI')
 library('ggplot2')
 library('rjson')
+library('reshape2')
+#library('data.table')
+library('sqldf')
+library('tidyr')
+library('dplyr')
+library('sfsmisc')# arrumar ^10 dos graficos
 
 con <- dbConnect(SQLite(), dbname="tcc_statistics.sqlite")
 #con <- dbConnect(SQLite(), dbname="all.sqlite")
 
-# Define server logic for random distribution application
-shinyServer(function(input, output) {
+# shinyServer(function(input, output) {
+#
+#   #pg 71 distribuicao empirica de requicoes por ip
+#   consultaplot <-dbSendQuery(con, "select questions from DNS_CLIENT;")
+#   consultaplot1 <- fetch(consultaplot)
+#   consultaplot1 <- na.omit(consultaplot1)
+#   consultaplot1 <- consultaplot1[[1]]
+#
+#   tableplot2 <- data.frame(consultaplot1)
+#
+#   output$plot <- renderPlot({
+#     plot(ecdf(consultaplot1))
+#   })
+#
+#   output$summary <- renderPrint({
+#     summary(ecdf(consultaplot1))
+#   })
+#
+#    output$table <- renderTable({
+#      data.frame(x=consultaplot1)
+#    })
+#
+# })
 
-  #pg 71 distribuicao empirica de requicoes por ip
+ui <- fluidPage()
 
-  consultaplot <-dbSendQuery(con, "select questions from DNS_CLIENT;")
-  consultaplot1 <- fetch(consultaplot)
-  consultaplot1 <- na.omit(consultaplot1)
-  consultaplot1 <- consultaplot1[[1]]
+server <- function(input, output) {
 
-  #cat("n = ", length(consultaplot1))
-  tableplot2 <- data.frame(consultaplot1)
-
-  #png('plot-2.png')
-  #plot(ecdf(consultaplot1))
-  ##ggplot(tableplot2,aes(x = consultaplot1)) + stat_ecdf(aes(colour = consultaplot1))
-  #dev.off()
-
-  # # Reactive expression to generate the requested distribution. This is
-  # # called whenever the inputs change. The output renderers defined
-  # # below then all used the value computed from this expression
-  # data <- reactive({
-  #   dist <- switch(input$dist,
-  #                  norm = rnorm,
-  #                  unif = runif,
-  #                  lnorm = rlnorm,
-  #                  exp = rexp,
-  #                  rnorm)
-  #
-  #   dist(input$n)
-  # })
-
-  # Generate a plot of the data. Also uses the inputs to build the
-  # plot label. Note that the dependencies on both the inputs and
-  # the data reactive expression are both tracked, and all expressions
-  # are called in the sequence implied by the dependency graph
-  output$plot <- renderPlot({
-    plot(ecdf(consultaplot1))
+  output$coolplot <- renderPlot({
+    if (is.null(filtered())) {
+      return()
+    }
+    ggplot(filtered(), aes(Alcohol_Content)) +
+      geom_histogram()
   })
 
-
-  # Generate a summary of the data
-
-  output$summary <- renderPrint({
-    summary(ecdf(consultaplot1))
+  output$results <- renderTable({
+    filtered()
   })
-  #
-  # # Generate an HTML table view of the data
-   output$table <- renderTable({
-     data.frame(x=consultaplot1)
-   })
+}
 
-  # This would go in shinyServer()
-  # output$myPlot <- renderPlot({
-  # # Your plotting code here
-  #     plot(ecdf(consultaplot1))
-  # })
-
-})
+shinyApp(ui = ui, server = server)
